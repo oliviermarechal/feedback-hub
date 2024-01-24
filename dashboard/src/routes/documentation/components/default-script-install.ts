b4c05251f80483ad83376547e85ae53cdc0dd46e
Olivier Maréchal <o.marechal@icloud.com>
2024-01-24 21:21:46 +0100

    Fix documentation

--- dashboard/src/routes/documentation/components/default-script-install.ts
+++ dashboard/src/routes/documentation/components/default-script-install.ts
@@ -1,4 +1,4 @@
 export const getDefaultScriptInstall = () =>  `
-<link rel="stylesheet" type="text/css" href="https://unpkg.com/insight-hunt@0.0.8/dist/bundle.css">
-<script src="https://unpkg.com/insight-hunt@0.0.8/dist/bundle.js"></script>
+<link rel="stylesheet" type="text/css" href="https://unpkg.com/insight-hunt@0.0.9/dist/bundle.css">
+<script src="https://unpkg.com/insight-hunt@0.0.9/dist/bundle.js"></script>
 `
